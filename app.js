4f1ff0ad05148348a4a856ad0bc32119faf92462
PILPROD <popov.pilprod@gmail.com>
2021-05-17 15:36:12 +0300

    Enable CORS for all v2 API

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const routes = require('./routes');
+var cors = require('cors');
 // const path = require('path'),
 // const dotenv = require('dotenv').config();
 var app = express()
 var host = process.env.HOST
 var port = process.env.PORT
+
+// Enable CORS
+var allowDomain = ['https://www.pilprod.com', 'https://ww2.technobox67.ru']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowDomain.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
+app.options('/v2', cors(corsOptionsDelegate)) // enable pre-flight request for DELETE request
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
--- controllers/v2/forms.controller.js
+++ controllers/v2/forms.controller.js
@@ -1,4 +1,4 @@
-const express = require('express');
+// const express = require('express');
 const nodemailer = require('nodemailer');
 
 class FormsController {
